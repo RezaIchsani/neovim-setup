092fdceb1662e9cbaa5171675fcd17937cf480cd
RezaIchsani <reza.ichsany@gmail.com>
2023-03-13 22:49:49 +0700

    edit nvim-notify

--- after/plugin/nvim-notify.rc.lua
+++ after/plugin/nvim-notify.rc.lua
@@ -1,4 +1,6 @@
-local nvim_notify = require('notify')
+local status, nvim_notify = pcall(require, 'notify')
+if (not status) then return end
+
 nvim_notify.setup {
   --animation style
   stage = 'fade_in_slide_out',
