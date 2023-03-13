d0407fff82906cab7b21c099d962963b2108c64c
RezaIchsani <reza.ichsany@gmail.com>
2023-03-13 22:41:32 +0700

    add nvim-nofify

---
+++ after/plugin/nvim-notify.rc.lua
@@ -0,0 +1,10 @@
+local nvim_notify = require('notify')
+nvim_notify.setup {
+  --animation style
+  stage = 'fade_in_slide_out',
+  -- default timeout for notification
+  timeout = 1500,
+  background_color = '#2e3440',
+}
+
+vim.notify = nvim_notify
